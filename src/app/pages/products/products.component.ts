fc6575770cc569528fb97c7371282ce9bb27bbeb
David Pereiro Amez <323628@bilbao.eus>
2026-01-15 09:55:28 +0100

    Initial commit

---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+import { CurrencyPipe } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit {
+  
+  productList: IProduct[] = [];
+  private _apiService = inject(ApiService);
+  private _router = inject(Router);
+  ngOnInit(): void {
+    this._apiService.getProducts().subscribe((data: IProduct[]) => {
+      this.productList = data;
+    });
+  }
+
+  navigate(id:number): void{
+    this._router.navigate(['/products', id]);
+  }
+}
